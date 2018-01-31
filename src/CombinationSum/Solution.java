b36e8dd0f07592e15577148fe302fe9956585708
nicppa <449117059@qq.com>
2018-01-31 21:55:18 +0800

    2018-01-31

---
+++ src/CombinationSum/Solution.java
@@ -0,0 +1,25 @@
+package CombinationSum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(candidates);
+        getResult(result,new ArrayList<Integer>(),candidates,target,0);
+        return result;
+    }
+    public void getResult(List<List<Integer>> result,List<Integer> cur,int[] candidates,int target,int pos){
+        if(target > 0){
+            for(int i = pos; i < candidates.length && target >= candidates[i];i++){
+                cur.add(candidates[i]);
+                getResult(result,cur,candidates,target-candidates[i],i);
+                cur.remove(candidates[i]);
+            }
+        }else if(target == 0){
+            result.add(cur);
+        }
+    }
+}
