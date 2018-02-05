ebfad3ebd8d5a7442c71fd2b16519bdb471a4391
nicppa <449117059@qq.com>
2018-02-05 21:11:09 +0800

    018-02-05

--- src/TrappingRainWater/Solution.java
+++ src/TrappingRainWater/Solution.java
@@ -2,6 +2,22 @@ package TrappingRainWater;
 
 public class Solution {
     public int trap(int[] height) {
-
+        int left = 0;
+        int right = height.length - 1;
+        int leftmax = 0;
+        int rightmax = 0;
+        int count = 0;
+        while(left <= right ){
+            leftmax = Math.max(leftmax,height[left]);
+            rightmax = Math.max(rightmax,height[right]);
+            if(leftmax > rightmax){
+                count+=(rightmax-height[right]);
+                right--;
+            }else{
+                count+=(leftmax-height[left]);
+                left++;
+            }
+        }
+        return count;
     }
 }
